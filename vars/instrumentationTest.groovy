73db685784e67a2fb317d039255c8baebce9831b
jpollak <jozsef.pollak@emarsys.com>
2017-03-01 19:52:26 +0100

    instrumentation test with optional locking and archiving

--- vars/instrumentationTest.groovy
+++ vars/instrumentationTest.groovy
@@ -1,11 +1,23 @@
-def call(){
-    try{
-        sh './gradlew cAT'
-    } catch(e){
-        currentBuild.result = 'FAILURE'
-        throw e
-    } finally {
-        junit '**/test-results/**/*.xml'
-        archiveArtifacts '**/test-results/**/*.xml'
+def call(Map args){
+    Closure cAT = {
+        try {
+            sh './gradlew cAT'
+        } catch (e) {
+            currentBuild.result = 'FAILURE'
+            throw e
+        } finally {
+            if(args['andArchive']){
+                archiveArtifacts
+                junit args['andArchive']
+                archiveArtifacts args['andArchive']
+            }
+        }
+    }
+    if(args['withLock']){
+        lock(args['withLock']){
+            cAT()
+        }
+    }else{
+        cAT()
     }
 }
