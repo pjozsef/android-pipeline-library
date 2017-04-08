a76668262a98338ea893f59d5d1f3df8a9982e44
jpollak <jozsef.pollak@emarsys.com>
2017-04-09 01:42:18 +0200

    fix(reboot): fix missing androidhome

--- vars/rebootDevices.groovy
+++ vars/rebootDevices.groovy
@@ -3,7 +3,7 @@ import com.github.pjozsef.DeviceLister
 
 def call(Map args){
     Closure reboot = {
-        def devices = args['devices'] ?: new DeviceLister(androidHome).availableDevices()
+        def devices = args['devices'] ?: new DeviceLister(env.ANDROID_HOME).availableDevices()
         new DeviceCommander(env.ANDROID_HOME, devices).execute("reboot")
     }
     Closure wait = {
