ab4cbbf25772aed9faa10edc95307c4fdcfc7469
pjozsef <pollak.jozsef1@gmail.com>
2017-06-16 11:31:41 +0200

    Provide alternative action for deviceCount step

---
+++ vars/deviceCount.groovy
@@ -0,0 +1,26 @@
+import com.github.pjozsef.DeviceLister
+
+def call(Map args) {
+    def expectedCount = args['shouldBe']
+    if(expectedCount instanceof String){
+        expectedCount = expectedCount as Integer
+    }
+
+    def deviceLister = new DeviceLister(env.ANDROID_HOME)
+
+    echo deviceLister.devicesRawVerbose()
+
+    def devices = deviceLister.availableDevices()
+    def size = devices.size()
+
+    if(size != expectedCount) {
+        def message = "Currently available devices are ${size}, but should be ${expectedCount}!"
+        echo message
+        if(args['fail']) {
+            currentBuild.result = 'FAILURE'
+            throw new IllegalStateException(message)
+        } else if(args['action']){
+            args['action'](devices)    
+        }
+    }
+}
