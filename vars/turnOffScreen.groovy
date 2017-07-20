811740418f98b6cbdf439b82db6ef95d10013de8
jpollak <jozsef.pollak@emarsys.com>
2017-07-20 10:39:07 +0200

    fix(turn-off-screen): fix missing paranthesis

--- vars/turnOffScreen.groovy
+++ vars/turnOffScreen.groovy
@@ -1,5 +1,5 @@
 def call(){
-    for(device in devices(availableOnly: true) {
+    for(device in devices(availableOnly: true)) {
         def info1 = sh returnStdout: true, script: "${env.ANDROID_HOME}/platform-tools/adb -s $device.name shell dumpsys input_method"
         def info2 = sh returnStdout: true, script: "${env.ANDROID_HOME}/platform-tools/adb -s $device.name shell dumpsys power"
         if(info1.contains("mScreenOn=true") || info2.contains("Display Power: state=ON")){
