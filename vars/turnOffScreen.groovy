3d48ae0c40da86361a0823cb59e2ea1f031d9e6b
pjozsef <pollak.jozsef1@gmail.com>
2017-07-20 10:34:39 +0200

    fix(turn-off-screen): use device.name to get adb ID

--- vars/turnOffScreen.groovy
+++ vars/turnOffScreen.groovy
@@ -1,11 +1,10 @@
 def call(){
-    def devices = devices(availableOnly: true)
-    for(device in devices) {
-        def info1 = sh returnStdout: true, script: "${env.ANDROID_HOME}/platform-tools/adb -s $device shell dumpsys input_method"
-        def info2 = sh returnStdout: true, script: "${env.ANDROID_HOME}/platform-tools/adb -s $device shell dumpsys power"
+    for(device in devices(availableOnly: true) {
+        def info1 = sh returnStdout: true, script: "${env.ANDROID_HOME}/platform-tools/adb -s $device.name shell dumpsys input_method"
+        def info2 = sh returnStdout: true, script: "${env.ANDROID_HOME}/platform-tools/adb -s $device.name shell dumpsys power"
         if(info1.contains("mScreenOn=true") || info2.contains("Display Power: state=ON")){
-            sh "${env.ANDROID_HOME}/platform-tools/adb -s $device shell input keyevent KEYCODE_HOME"
-            sh "${env.ANDROID_HOME}/platform-tools/adb -s $device shell input keyevent KEYCODE_POWER"
+            sh "${env.ANDROID_HOME}/platform-tools/adb -s $device.name shell input keyevent KEYCODE_HOME"
+            sh "${env.ANDROID_HOME}/platform-tools/adb -s $device.name shell input keyevent KEYCODE_POWER"
         }
     }
 }
